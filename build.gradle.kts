41e271971204fd387dbfc062f02b17490a90afde
Danial Kassym <danialkassym@gmail.com>
2026-02-10 20:35:43 +0500

    Lab1 commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.springproject"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	//testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.postgresql:postgresql:42.7.7")
+	implementation ("org.springframework.data:spring-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
