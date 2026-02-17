39e61502135843a2dc1dfc5a2b9b429d0f525f75
Danial Kassym <danialkassym@gmail.com>
2026-02-17 22:38:56 +0500

    Lab2 done final

--- src/main/java/com/springproject/Spring/CRUD/CRUDControllers.java
+++ src/main/java/com/springproject/Spring/CRUD/CRUDControllers.java
@@ -6,55 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-/*
-@Controller
-public class Controllers {
-    private UserRepository userRepository;
-
-    @Autowired
-    public Controllers(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping("/users")
-    public String list(
-            @RequestParam(name="id", required = false) Long id,
-            Model model){
-
-        Users form = id != null? userRepository.getById(id) : new Users();
-
-        model.addAttribute("users", userRepository.findAll());
-        model.addAttribute("form", form);
-        model.addAttribute("editMode", id != null);
-        return "users";
-    }
-
-    @PostMapping("/user/create/")
-    public String CreateUser(@PathVariable("username") String username) {
-        LocalDateTime localDateTime = LocalDateTime.now().plusMinutes(15);
-        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-        Users user = new Users(username,date);
-        userRepository.save(user);
-        return "redirect:/users";
-    }
-
-    @PutMapping("/user/update/{username}")
-    public String UpdateUser(@PathVariable("username") String username,@RequestParam("newuser") String newUsername) {
-        Users users = userRepository.findByname(username);
-        if (users == null){
-            return "redirect:/users";
-        }
-        users.setName(newUsername);
-        return "redirect:/users";
-    }
-
-    @DeleteMapping("/user/delete/{id}")
-    public String DeleteUser(@PathVariable("id") Long id) {
-        userRepository.deleteById(id);
-
-        return "redirect:/users";
-    }
-}*/
 
 @Controller
 @RequestMapping("/users")
